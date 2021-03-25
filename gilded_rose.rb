29a84c19d28d2ed788e2167c32c4413bf33f2a86
Justin Burris <justinbburris@gmail.com>
2021-03-25 12:17:18 -0400

    final wrap

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,69 +1,130 @@
 def update_quality(items)
   items.each do |item|
-    next if item.name == 'Sulfuras, Hand of Ragnaros'
+    modify_quality(item)
+  end
+end
 
-    if item.name == 'Conjured Mana Cake'
-      if item.quality > 0
-        if item.sell_in > 0
-          item.quality -= 2
-        else
-          item.quality -= 4
-        end
-      end
+def update_quality2(items)
+  items.each do |item|
+    modifier = QualityModifer.fetch_modifer(item)
+    modifer.adjust_quality!
+  end
+end
 
-      item.sell_in -= 1
-      next
-    end
-
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-      if item.sell_in < 11
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-      if item.sell_in < 6
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-
-      if item.quality < 50
-        item.quality += 1
-      end
-
-      item.sell_in -= 1
-
-      if item.sell_in < 0
-        item.quality = item.quality - item.quality
-      end
-
-      next
-    end
-
-    if item.name != 'Aged Brie'
-      if item.quality > 0
-        item.quality -= 1
-      end
+class QualityModifer
+  def self.fetch_modifer(item)
+    case(item.name)
+    when 'Sulfuras, Hand of Ragnaros'
+      SulfurasModifer.new(item)
+    when 'Conjured Mana Cake'
+      ConjuredModifer.new(item)
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      PassModifer.new(item)
+    when 'Aged Brie'
+      BrieModifer.new(item)
     else
-      if item.quality < 50
-        item.quality += 1
-      end
+      NormalModifer.new(item)
+    end
+  end
+end
+
+def modify_quality(item)
+  case(item.name)
+  when 'Sulfuras, Hand of Ragnaros'
+    return
+  when 'Conjured Mana Cake'
+    modify_conjured(item)
+  when 'Backstage passes to a TAFKAL80ETC concert'
+    modify_pass(item)
+  when 'Aged Brie'
+    modify_brie(item)
+  else
+    modify_normal(item)
+  end
+end
+
+class NormalModifer
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def adjust_quality!
+    if item.quality > 0
+      item.quality -= 1
     end
 
     item.sell_in -= 1
 
-    # handles twice as fast deprecation
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.quality > 0
-          item.quality -= 1
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
+      if item.quality > 0
+        item.quality -= 1
       end
     end
+  end
+end
+
+def modify_normal(item)
+  if item.quality > 0
+    item.quality -= 1
+  end
+
+  item.sell_in -= 1
+
+  return if item.sell_in > 0
+
+  if item.quality > 0
+    item.quality -= 1
+  end
+end
+
+def modify_brie(item)
+  if item.quality < 50
+    item.quality += 1
+  end
+
+  item.sell_in -= 1
+
+  if item.sell_in < 0
+    if item.quality < 50
+      item.quality += 1
+    end
+  end
+end
+
+def modify_conjured(item)
+  if item.quality > 0
+    if item.sell_in > 0
+      item.quality -= 2
+    else
+      item.quality -= 4
+    end
+  end
+
+  item.sell_in -= 1
+end
+
+def modify_pass(item)
+  if item.sell_in < 11
+    if item.quality < 50
+      item.quality += 1
+    end
+  end
+  if item.sell_in < 6
+    if item.quality < 50
+      item.quality += 1
+    end
+  end
+
+  if item.quality < 50
+    item.quality += 1
+  end
+
+  item.sell_in -= 1
+
+  if item.sell_in < 0
+    item.quality = item.quality - item.quality
   end
 end
 
